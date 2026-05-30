7b8809a58efba22c38e65fb2b9865c181538d9f3
Jade Vieux <jade.vieux@sio-carriat.com>
2026-05-30 19:32:15 +0200

    changement connexion bdd

--- Classes/CDao.php
+++ Classes/CDao.php
@@ -11,8 +11,8 @@ class CDao {
     public function __construct() {
 
         // Connexion à la BDD
-        $this->pdo = new PDO('mysql:host=localhost;dbname=musicvault;charset=utf8', 'root', 'P@ssw0rd');
-        // $this->pdo = new PDO('mysql:host=172.20.126.2;dbname=musicvault;charset=utf8', 'apiUser', 'Jadeuser123@!!');
+        // $this->pdo = new PDO('mysql:host=localhost;dbname=musicvault;charset=utf8', 'root', 'P@ssw0rd');
+        $this->pdo = new PDO('mysql:host=172.20.126.2;dbname=musicvault;charset=utf8', 'apiUser', 'Jadeuser123@!!');
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     }
